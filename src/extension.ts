5ac9e791f1f02e91998eb337a34bf441a0975615
Mateja Petrovic <mateja176@gmail.com>
2019-03-02 14:08:42 +0100

    Formats html indentation

--- src/extension.ts
+++ src/extension.ts
@@ -27,18 +27,19 @@ export function activate(context: vscode.ExtensionContext) {
         .split(":")[1];
 
       const htmlContent = `<!DOCTYPE html>
-  <html lang="en">
+<html lang="en">
   <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-      <title>Document</title>
-      <link rel="stylesheet" href="app.css" />
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Document</title>
+    <link rel="stylesheet" href="app.css" />
   </head>
   <body>
-      <script src="app.js"></script>
+    <script src="app.js"></script>
   </body>
-  </html>`;
+</html>
+`;
 
       fs.writeFile(path.join(folderPath, "index.html"), htmlContent, err => {
         if (err) {
